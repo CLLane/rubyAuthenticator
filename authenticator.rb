1902415fa18255decde7412cdac41f1c6f8bd858
Chris Lane <pankokrumbs@gmail.com>
2020-05-18 16:25:58 -0600

    create count and incriment method

--- authenticator.rb
+++ authenticator.rb
@@ -1,7 +1,20 @@
 users = [
   {name: 'Chris', password: '12345'}, {name:'Joe', password:'joeblow'}, {name:'Sam', password: 'password'}, {name: 'Steve', password: 'password1'}
 ]
+count = 0
+
+def incriment 
+  count++
+end
 
 puts 'Welcome to the authenticator program please sign in :'
 
 25.times{ print "-"}
+puts
+puts "Username :"
+user_name = gets.chomp
+user_name_check = users.select{|user| user[:name] == user_name}
+if user_name_check.length > 0 puts 'Password'
+else puts 'Press n to quit or any other key to continue'
+end
+
