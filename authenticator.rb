480496a378dda2737a5448ef4da2a07d0565d14b
Chris Lane <pankokrumbs@gmail.com>
2020-05-18 15:50:10 -0600

    Welcome user script

--- authenticator.rb
+++ authenticator.rb
@@ -2,3 +2,6 @@ users = [
   {name: 'Chris', password: '12345'}, {name:'Joe', password:'joeblow'}, {name:'Sam', password: 'password'}, {name: 'Steve', password: 'password1'}
 ]
 
+puts 'Welcome to the authenticator program please sign in :'
+
+25.times{ print "-"}
