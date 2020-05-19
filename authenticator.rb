c9918a120faa1b29916818c984a1fca6e3ed1f4e
Chris Lane <pankokrumbs@gmail.com>
2020-05-19 12:28:40 -0600

    add password to authentication

--- authenticator.rb
+++ authenticator.rb
@@ -1,20 +1,6 @@
 users = [
   {name: 'Chris', password: '12345'}, {name:'Joe', password:'joeblow'}, {name:'Sam', password: 'password'}, {name: 'Steve', password: 'password1'}
 ]
-count = 0
-def incriment()
-  puts 'Press n to quit or any other key to continue'
-  count += 1
-  key = gets.chomp;
-end
-
-def passwordCheck(pass) 
-  if pass === password
-    puts user_name_check 
-  else
-    incriment();
-  end
-end
 
 
  
@@ -23,14 +9,17 @@ puts 'Welcome to the authenticator program please sign in :'
 25.times{ print "-"}
 puts
 puts "Username :"
-user_name = gets.chomp
-user_name_check = users.select{|user| user[:name] == user_name}
-if user_name_check.length() > 0 
-  puts 'Password :'
-  password = gets.chomp
-else 
-  incriment()
-  end
+# user_name = gets.chomp
+# user_name_check = users.select{|user| user[:name] == user_name}
+attempts = 1
 
-if password == user_name_check[]
-p count
+while attempts < 4
+  print "Username: "
+  username = gets.chomp
+  print "Password: "
+  password = gets.chomp
+  puts "Press n to quit or any other key to continue"
+  input = gets.chomp.downcase
+  break if input == 'n'
+  attempts += 1
+end
